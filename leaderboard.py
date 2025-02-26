89540160d4143370bdc9279ea38b909a017f40bc
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2025-02-26 05:16:55 +0000

    Update leaderboard.py

--- leaderboard.py
+++ leaderboard.py
@@ -2,7 +2,7 @@ import os
 from pymongo import MongoClient
 
 # MongoDB connection
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://quiz:quiz123@+@cluster0.yzjqw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 client = MongoClient(MONGO_URI)
 db = client["telegram_bot"]
 leaderboard_collection = db["leaderboard"]
