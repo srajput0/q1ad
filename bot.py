d7e297573f56fd3eccdb36097c985acbd1263eb4
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2025-03-04 11:56:11 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from telegram.ext import (
     Updater, CommandHandler, CallbackQueryHandler, CallbackContext, PollAnswerHandler
 )
 from chat_data_handler import load_chat_data, save_chat_data, add_served_chat, add_served_user, get_active_quizzes
-from quiz_handler import send_quiz, send_quiz_immediately, handle_poll_answer
+from quiz_handler import send_quiz, send_quiz_immediately, send_quiz_to_channel, handle_poll_answer
 from admin_handler import broadcast
 from leaderboard_handler import get_user_score, get_top_scores
 from datetime import datetime
@@ -87,7 +87,7 @@ def button(update: Update, context: CallbackContext):
         elif query.data == 'prequiz' and update.effective_chat.type != 'private':
             query.edit_message_text(text="The Prequiz option is only available in private chats.")
             return
-        elif query.data == 'channel_quiz' and update.effective_chat.type != 'private':
+        elif query.data == 'channel_quiz' and update.effective_chat.type != 'channel':
             query.edit_message_text(text="The Channel Quiz option is only available in channels.")
             return
 
