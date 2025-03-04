26908bb6019b041186c2b05e7d64d9dea48039de
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2025-03-04 12:35:16 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -99,7 +99,7 @@ def button(update: Update, context: CallbackContext):
 
         # Inline buttons for interval selection
         keyboard = [
-            [InlineKeyboardButton("30 sec", callback_data='interval_10')],
+            [InlineKeyboardButton("30 sec", callback_data='interval_30')],
             [InlineKeyboardButton("1 min", callback_data='interval_60')],
             [InlineKeyboardButton("5 min", callback_data='interval_300')],
             [InlineKeyboardButton("10 min", callback_data='interval_600')],
@@ -185,7 +185,7 @@ def set_channel(update: Update, context: CallbackContext):
 
     # Inline buttons for interval selection
     keyboard = [
-        [InlineKeyboardButton("30 sec", callback_data='interval_10')],
+        [InlineKeyboardButton("30 sec", callback_data='interval_30')],
         [InlineKeyboardButton("1 min", callback_data='interval_60')],
         [InlineKeyboardButton("5 min", callback_data='interval_300')],
         [InlineKeyboardButton("10 min", callback_data='interval_600')],
