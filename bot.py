3b49f4d51349371648a0e0de2ae382d50869a0f9
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2025-04-25 09:22:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -73,7 +73,7 @@ def start_command(update: Update, context: CallbackContext):
 
     # Send welcome message with main menu buttons
     update.message.reply_text(
-        "*Pinnacle 7th Edition*\n\nWelcome to the Pinnacle 7th edition Quiz Bot!\nThis is a Quiz Bot made by *Pinnacle Publication.*\nThis can ask two Exams PYQ's.\n\n*SSC PINNACLE 7th EDITION*\n*RRB PINNACLE Latest EDITION*\n\nChoose the option for proceed further :",
+        "*Pinnacle 7th Edition*\n\nWelcome to the Pinnacle 7th edition Quiz Bot! This is a Quiz Bot made by *Pinnacle Publication.*\nThis can ask two Exams PYQ's.\n\n*[SSC PINNACLE 7th EDITION]*\n*[RRB PINNACLE Latest EDITION]*\n\nChoose the option for proceed further :",
         reply_markup=reply_markup, parse_mode="Markdown"
     )
 
