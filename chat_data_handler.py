02bc8845a46bca4a3b1d0e552f5fa03b8aa9740f
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2025-05-02 10:16:27 +0530

    Update chat_data_handler.py

--- chat_data_handler.py
+++ chat_data_handler.py
@@ -3,7 +3,8 @@ from pymongo import MongoClient
 
 # MongoDB connection
 # MONGO_URI = "mongodb+srv://asrushfig:2003@cluster0.6vdid.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-MONGO_URI = "mongodb+srv://tigerbundle282:tTaRXh353IOL9mj2@testcookies.2elxf.mongodb.net/?retryWrites=true&w=majority&appName=Testcookies"
+# MONGO_URI = "mongodb+srv://tigerbundle282:tTaRXh353IOL9mj2@testcookies.2elxf.mongodb.net/?retryWrites=true&w=majority&appName=Testcookies"
+MONGO_URI = "mongodb+srv://2004:2005@cluster0.6vdid.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 client = MongoClient(MONGO_URI)
 db = client["telegram_bot"]
