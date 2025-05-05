43d68ca285f6c96427ba9c765edf4618fd6bf6e2
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2025-05-05 21:56:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -331,15 +331,11 @@ def button(update: Update, context: CallbackContext):
                 f"❌ *Incorrect Answers*: {stats['incorrect_answers']}\n\n"
                 f" */start - Use this to start*"
                 )
-            keyboard = [
-            [InlineKeyboardButton("Back", callback_data='back_to_main_menu')]]
-            reply_markup = InlineKeyboardMarkup(keyboard)
-            query.edit_message_text(text=f"Available Commands:\n{commands_description}", reply_markup=reply_markup)
-            # keyboard = [
-            # [InlineKeyboardButton("Back", callback_data='back_to_main_menu')]
-            # ]
-            # reply_markup = InlineKeyboardMarkup(keyboard)
-            # query.edit_message_text(message, parse_mode="Markdown")
+        keyboard = [
+        [InlineKeyboardButton("Back", callback_data='back_to_main_menu')]
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        query.edit_message_text(message, parse_mode="Markdown")
             
         except Exception as e:
             logger.error(f"Error in 'show_stats' button: {str(e)}")
