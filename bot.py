ac39d38e51894105116d905ba6f6eef253c3270e
DarkAarush <128771887+DarkAarush@users.noreply.github.com>
2025-05-11 11:26:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -712,11 +712,11 @@ def main():
     updater = Updater(
         bot=bot,
         use_context=True,
-        workers=28,
+        workers=4,
         request_kwargs={
             'read_timeout': 10,
             'connect_timeout': 10,
-            'connect_pool_size': 250,  # Match this with workers count
+            'connect_pool_size': 8,  # Match this with workers count
             'connect_retries': 3,
             'pool_timeout': 30
         }
